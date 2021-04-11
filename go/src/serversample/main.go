566ff287cbf5fb7d24a2c17ea9d668fade84b9ba
takuya <nagataku021@outlook.jp>
2021-04-11 22:00:38 +0900

    mongoから取得したドキュメントをwebに表示

--- go/src/serversample/main.go
+++ go/src/serversample/main.go
@@ -2,10 +2,11 @@ package main
 
 import (
 	"fmt"
+	"encoding/json"
 	"net/http"
-	"strings"
 	"context"
 	"time"
+	"github.com/gorilla/mux"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
@@ -13,6 +14,48 @@ import (
 
 // mani関数が最初に実行される
 func main() {
+
+	StartWebServer()
+
+}
+
+func rootPage(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Welcome to the Go Api Server")
+	fmt.Println("Root endpoint is hooked!")
+}
+
+func StartWebServer() error {
+	fmt.Println("Rest API with Mux Routers")
+	router := mux.NewRouter().StrictSlash(true)
+	// router.HandleFunc({ エンドポイント }, { レスポンス関数 }).Methods({ リクエストメソッド（複数可能） })
+	router.HandleFunc("/", rootPage)
+	router.HandleFunc("/items", fetchAllItems).Methods("GET")
+	// router.HandleFunc("/item/{id}", fetchSingleItem).Methods("GET")
+
+	// router.HandleFunc("/item", createItem).Methods("POST")
+	// router.HandleFunc("/item/{id}", deleteItem).Methods("DELETE")
+	// router.HandleFunc("/item/{id}", updateItem).Methods("PUT")
+
+	return http.ListenAndServe(fmt.Sprintf(":%d", 8080), router)
+}
+type ItemParams struct {
+	Id           string    `json:"id"`
+	JanCode      string    `json:"jan_code,omitempty"`
+	ItemName     string    `json:"item_name,omitempty"`
+	Price        int       `json:"price,omitempty"`
+	CategoryId   int       `json:"category_id,omitempty"`
+	SeriesId     int       `json:"series_id,omitempty"`
+	Stock        int       `json:"stock,omitempty"`
+	Discontinued bool      `json:"discontinued"`
+	ReleaseDate  time.Time `json:"release_date,omitempty"`
+	CreatedAt    time.Time `json:"created_at"`
+	UpdatedAt    time.Time `json:"updated_at"`
+	DeletedAt    time.Time `json:"deleted_at"`
+}
+
+var items []*ItemParams
+
+func fetchAllItems(w http.ResponseWriter, r *http.Request) {
 	type dataType struct {
 		Rid     string `bson:"rid"`
 		Keyword string `bson:"keyword"`
@@ -46,5 +89,39 @@ func main() {
 		var ret dataType
 		cur.Decode(&ret)
 		fmt.Println(ret)
+	}
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(data)
+}
+func init() {
+	items = []*ItemParams{
+			&ItemParams{
+					Id:           "1",
+					JanCode:      "327390283080",
+					ItemName:     "item_1",
+					Price:        2500,
+					CategoryId:   1,
+					SeriesId:     1,
+					Stock:        100,
+					Discontinued: false,
+					ReleaseDate:  time.Now(),
+					CreatedAt:    time.Now(),
+					UpdatedAt:    time.Now(),
+					DeletedAt:    time.Now(),
+			},
+			&ItemParams{
+					Id:           "2",
+					JanCode:      "3273902878656",
+					ItemName:     "item_2",
+					Price:        1200,
+					CategoryId:   2,
+					SeriesId:     2,
+					Stock:        200,
+					Discontinued: false,
+					ReleaseDate:  time.Now(),
+					CreatedAt:    time.Now(),
+					UpdatedAt:    time.Now(),
+					DeletedAt:    time.Now(),
+			},
 	}
 }
