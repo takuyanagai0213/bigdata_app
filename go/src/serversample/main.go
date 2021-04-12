2b322736c4c4ca7d1396e8ce98bbf8936b5f2eed
takuya <nagataku021@outlook.jp>
2021-04-12 22:08:02 +0900

    複数表示をできるようにした

--- go/src/serversample/main.go
+++ go/src/serversample/main.go
@@ -32,34 +32,18 @@ func StartWebServer() error {
 	router.HandleFunc("/items", fetchAllItems).Methods("GET")
 	// router.HandleFunc("/item/{id}", fetchSingleItem).Methods("GET")
 
-	// router.HandleFunc("/item", createItem).Methods("POST")
+	router.HandleFunc("/newitem", createItem).Methods("GET")
 	// router.HandleFunc("/item/{id}", deleteItem).Methods("DELETE")
 	// router.HandleFunc("/item/{id}", updateItem).Methods("PUT")
 
 	return http.ListenAndServe(fmt.Sprintf(":%d", 8080), router)
 }
-type ItemParams struct {
-	Id           string    `json:"id"`
-	JanCode      string    `json:"jan_code,omitempty"`
-	ItemName     string    `json:"item_name,omitempty"`
-	Price        int       `json:"price,omitempty"`
-	CategoryId   int       `json:"category_id,omitempty"`
-	SeriesId     int       `json:"series_id,omitempty"`
-	Stock        int       `json:"stock,omitempty"`
-	Discontinued bool      `json:"discontinued"`
-	ReleaseDate  time.Time `json:"release_date,omitempty"`
-	CreatedAt    time.Time `json:"created_at"`
-	UpdatedAt    time.Time `json:"updated_at"`
-	DeletedAt    time.Time `json:"deleted_at"`
+
+type dataType struct {
+	Rid     string `bson:"rid"`
+	Keyword string `bson:"keyword"`
 }
-
-var items []*ItemParams
-
-func fetchAllItems(w http.ResponseWriter, r *http.Request) {
-	type dataType struct {
-		Rid     string `bson:"rid"`
-		Keyword string `bson:"keyword"`
-	}
+func createItem(w http.ResponseWriter, r *http.Request) {	
 
 	// 挿入するデータを作成
 	data := dataType{
@@ -79,8 +63,19 @@ func fetchAllItems(w http.ResponseWriter, r *http.Request) {
 	result, err := collection.InsertOne(context.Background(), data)
 	print(err)
 	print(result)
+}
 
+func fetchAllItems(w http.ResponseWriter, r *http.Request) {
+	clientOptions := options.Client().ApplyURI("mongodb://mongodb:27017")
 	
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	client, err := mongo.Connect(ctx, clientOptions)
+	if err != nil {
+		panic(err)
+	}
+	
+	collection := client.Database("test").Collection("users")
 	cur, err := collection.Find(context.Background(), bson.M{
 		"keyword": "俺のキーワード",
 		})
@@ -89,39 +84,7 @@ func fetchAllItems(w http.ResponseWriter, r *http.Request) {
 		var ret dataType
 		cur.Decode(&ret)
 		fmt.Println(ret)
-	}
-	w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(data)
-}
-func init() {
-	items = []*ItemParams{
-			&ItemParams{
-					Id:           "1",
-					JanCode:      "327390283080",
-					ItemName:     "item_1",
-					Price:        2500,
-					CategoryId:   1,
-					SeriesId:     1,
-					Stock:        100,
-					Discontinued: false,
-					ReleaseDate:  time.Now(),
-					CreatedAt:    time.Now(),
-					UpdatedAt:    time.Now(),
-					DeletedAt:    time.Now(),
-			},
-			&ItemParams{
-					Id:           "2",
-					JanCode:      "3273902878656",
-					ItemName:     "item_2",
-					Price:        1200,
-					CategoryId:   2,
-					SeriesId:     2,
-					Stock:        200,
-					Discontinued: false,
-					ReleaseDate:  time.Now(),
-					CreatedAt:    time.Now(),
-					UpdatedAt:    time.Now(),
-					DeletedAt:    time.Now(),
-			},
+		w.Header().Set("Content-Type", "application/json")
+		json.NewEncoder(w).Encode(ret)
 	}
 }
